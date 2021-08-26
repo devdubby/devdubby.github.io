ea22c0d81f2780e8ead2c45ed72e341fce8cf9c1
devdubby <devdubby@users.noreply.github.com>
2021-08-26 14:20:19 +0000

    Automated deployment: Thu Aug 26 14:20:19 UTC 2021 1fdef75b5c82df04c173039339271f73c0c7d7d9

--- sw.js
+++ sw.js
@@ -38,14 +38,14 @@ self.__precacheManifest = [
     "url": "framework-305b3707783ccc9d7ca6.js"
   },
   {
-    "url": "app-8f127b84f2d053ee49c7.js"
+    "url": "app-de22d9af4d126f6ecbb7.js"
   },
   {
     "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-15096bed533ed0ff8b58.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "15645a341cab2200729566e0d79533fa"
+    "revision": "ed8ae56daf9dab629ae257ffdc2215eb"
   },
   {
     "url": "polyfill-c5f50f2ad13861331c8a.js"
@@ -76,7 +76,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-8f127b84f2d053ee49c7.js`))) {
+  if (!resources || !(await caches.match(`/app-de22d9af4d126f6ecbb7.js`))) {
     return await fetch(event.request)
   }
 
