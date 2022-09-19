bebcbbe4ee8a75195835177f9b7edf142afa9daa
devdubby <cjsoung@naver.com>
2022-09-19 23:26:14 +0900

    revert package-lock

--- src/components/social-share/github-icon/index.jsx
+++ src/components/social-share/github-icon/index.jsx
@@ -5,7 +5,7 @@ import './index.scss'
 export const GitHubIcon = () => {
   return (
     <a
-      href="https://github.com/JaeYeopHan/felog"
+      href="https://github.com/devdubby/devdubby.github.io"
       className="github"
       aria-label="GitHub"
       target="_blank"
